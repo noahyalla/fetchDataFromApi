67475f0998098a6368a13b433122821249cad1d2
Noah Yalla <noahyalla@gmail.com>
2022-09-06 20:18:48 +1000

    first commit

---
+++ lib/api_service.dart
@@ -0,0 +1,20 @@
+import 'dart:developer';
+import 'package:http/http.dart' as http;
+import 'package:untitled/model.dart';
+import 'package:untitled/constants.dart';
+
+class ApiService {
+  Future<List<UserModel>?> getUsers() async {
+    try{
+      var url = Uri.parse(ApiConstants.baseUrl+ApiConstants.usersEndpoint);
+      var response = await http.get(url);
+      if(response.statusCode == 200){
+        List<UserModel> model = userModelFromJson(response.body);
+        return model;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return null;
+  }
+}
